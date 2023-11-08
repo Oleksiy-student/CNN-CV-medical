ecf7c3ef36aed68e6cf614fef36e67e6ba7fa08b
Marc Scattolin <marcscattolin@gmail.com>
2023-11-08 09:01:55 -0500

    make work on cuda

--- Train-model.py
+++ Train-model.py
@@ -3,13 +3,16 @@ from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
 from torchvision.transforms import ToTensor
 import torch
+import time
 
 
 def main():
-    model = resnet34(num_classes=3)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+    model = resnet34(num_classes=3).to(device)
+    print(f"Device: {device}")
+    
     torch.manual_seed(0)
+    # dataset = ImageFolder("/content/Colorectal Cancer/Dataset 1/Colorectal Cancer ", transform=ToTensor())
     dataset = ImageFolder("./Colorectal Cancer", transform=ToTensor())
     train_set, validation_set, test_set = torch.utils.data.random_split(dataset, [0.8, 0.1, 0.1])
     train_loader = DataLoader(train_set, shuffle=True, batch_size=64)
@@ -19,9 +22,9 @@ def main():
     
     optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=0.001)
     loss = torch.nn.CrossEntropyLoss()
+    num_epoch = 2
 
-    num_epoch = 10
-
+    start = time.time()
     for epoch in range(num_epoch):
         # Pass an epoch over the training data in batch_size chunks
         for features, labels in train_loader:
@@ -35,9 +38,10 @@ def main():
             # backprop and step
             l.backward()
             optimizer.step()
-            print(f"minibatch loss: {l}")
+            # print(f"minibatch loss: {l}")
         print(f"Epoch {epoch}: last batch loss: {l}")
-
+    
+    print(f"Time taken: {time.time() - start}")
 
 
 if __name__ == "__main__":
